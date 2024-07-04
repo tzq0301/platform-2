7c9dfc7cd85c17d8e56ba87b61440329afa5ec20
Zhaoqi Tong <522023330089@smail.nju.edu.cn>
2024-07-04 18:50:45 +0800

    feat(qtos-base): init

--- Makefile
+++ Makefile
@@ -3,8 +3,15 @@ MVN = mvn
 build-platform:
 	cd qtos-platform && $(MVN) package
 
+build-base:
+	cd qtos-base && $(MVN) package
+
 run-platform:
 	cd qtos-platform && $(MVN) spring-boot:run
 
+run-base:
+	cd qtos-base && $(MVN) spring-boot:run
+
 clean:
 	rm -rf qtos-platform/target/
+	rm -rf qtos-base/target/
---
+++ qtos-base/src/main/java/cn/edu/nju/ics/qtosbase/QtosBaseApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.ics.qtosbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QtosBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QtosBaseApplication.class, args);
+    }
+
+}
