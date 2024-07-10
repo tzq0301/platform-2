e5be02b2ad0ec796b87345ca9bc78d384cedb6dc
Zhaoqi Tong <522023330089@smail.nju.edu.cn>
2024-07-10 22:12:39 +0800

    feat(qtos-platform): CORS

---
+++ qtos-platform/src/main/java/cn/edu/nju/ics/qtosplatform/infrastructure/httpserver/SpringMvcConfig.java
@@ -0,0 +1,29 @@
+package cn.edu.nju.ics.qtosplatform.infrastructure.httpserver;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootConfiguration
+public class SpringMvcConfig implements WebMvcConfigurer {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+}
